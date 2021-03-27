74f0571af03ce8aa386a7b60c25d6420b6e82876
InfRandomness <infrandomness@gmail.com>
2021-03-27 20:07:50 +0100

    Initial commit

---
+++ src/cat.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+const args = @import("args");
+const fs = std.fs;
+const os = std.os;
+const target = std.Target;
+const File = fs.File;
+const heap = std.heap;
+const io = std.io;
+
+pub fn main() !void {
+    var argsAllocator = heap.page_allocator;
+    var fileContent: []u8 = undefined;
+    const stdout = io.getStdOut().writer();
+    const stdin = std.io.getStdIn().reader();
+
+    const ops = try args.parseForCurrentProcess(struct {
+        help: bool = false,
+
+        pub const shorthands = .{
+            .h = "help",
+        };
+    }, argsAllocator);
+    defer ops.deinit();
+
+    var gpa = heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    if (ops.positionals.len == 0) {
+
+        while (true) {
+            var line = try stdin.readUntilDelimiterAlloc(&gpa.allocator, '\n', std.math.maxInt(usize));
+            try stdout.print("{s}\n", .{line});
+        }
+    } else {
+        fileContent = try fetch_file_content(&gpa.allocator, ops.positionals[ops.positionals.len - 1]);
+        //try stdout.writer().print("filename = {s}", .{ops.positionals[ops.positionals.len]});
+        try stdout.print("{s}\n", .{fileContent});
+    }
+}
+
+fn fetch_file_content(allocator: *std.mem.Allocator, path: []const u8) ![]u8 {
+    const size = std.math.maxInt(usize);
+    const file = try std.fs.cwd().openFile(path, .{ .read = true });
+    defer file.close();
+    return try file.reader().readAllAlloc(allocator, size);
+}
